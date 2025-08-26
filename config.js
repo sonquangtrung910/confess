fe57c647cb076fe34cc1632f7a55156bd1a34e30
Tran Hoang Son <152843493+sonquangtrung910@users.noreply.github.com>
2025-08-26 09:55:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
     titleWeb: "private!!!",
     introTitle: 'Khánh Thi',
-    introDesc: `Trái đất vốn có 8 tỷ người
-    cớ sao mình lại đi lạc vào trái tim của 
-    bạn Kthi rồi mất nhỉ`,
+    introDesc: `Giữa hơn 8 tỷ người trên thế giới, 
+    vậy mà mình lại vô tình lạc vào trái tim 
+    của bạn Khánh Thi mất rồi 💖`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng mình đã thích bạn Khánh Thi rồi sao 💕',
-    desc: 'Ôi chết rồi… mình lỡ thích bạn Khánh Thi mất rồi. Thích từ lúc nào cũng chẳng hay 😌',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    title: 'Có phải mình đã lỡ thích bạn Khánh Thi rồi không nhỉ? 💕',
+    desc: 'Ôi… hình như mình đã thích bạn từ lúc nào chẳng hay, để rồi bây giờ chỉ muốn nói ra cho bạn biết thôi 😌',
+    btnYes: 'Tớ cũng thích cậu <33',
+    btnNo: 'Không đâu :3',
+    question: 'Trong hàng tỷ người ngoài kia, sao cậu lại chọn thích mình vậy? 🥰',
+    btnReply: 'Gửi cho cậu <3',
+    reply: 'Thích thì thích thôi, chẳng cần lý do gì cả <333',
+    mess: 'Mình biết mà 🥰. Yêu bạn nhiều thật nhiều 😘😘',
+    messDesc: 'Nhớ nhắn tin cho mình liền đó nha <3',
+    btnAccept: 'Okiiii luôn <3',
+    messLink: 'https://www.instagram.com/tran.hsonw/' // đổi thành link chat bạn muốn
 }
