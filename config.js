2e5f298ab61f996bb788dbdf349a54d9057f5f63
Tran Hoang Son <152843493+sonquangtrung910@users.noreply.github.com>
2025-08-26 09:49:59 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    titleWeb: "private!!!",
+    introTitle: 'Khánh Thi',
+    introDesc: `Trái đất vốn có 8 tỷ người
+    cớ sao mình lại đi lạc vào trái tim của 
+    bạn Kthi rồi mất nhỉ`,
+    btnIntro: '^^HiHi^^',
+    title: 'Phải chăng mình đã thích bạn Khánh Thi rồi sao 💕',
+    desc: 'Ôi chết rồi… mình lỡ thích bạn Khánh Thi mất rồi. Thích từ lúc nào cũng chẳng hay 😌',
+    btnYes: 'Thích lắm <33',
+    btnNo: 'Không nha :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    btnReply: 'Gửi cho bạn <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+}
